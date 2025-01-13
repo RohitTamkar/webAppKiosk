0f555a703c1da852282524211c71ed348468b0f0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-13 13:30:36 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/kiosk/loading_screenkiosknew/loading_screenkiosknew_widget.dart
+++ lib/pages/kiosk/loading_screenkiosknew/loading_screenkiosknew_widget.dart
@@ -38,7 +38,6 @@ class _LoadingScreenkiosknewWidgetState
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      await actions.clearCacheBeforeRun();
       await Future.delayed(const Duration(milliseconds: 2000));
       _model.outletgetdirect = await queryOutletRecordOnce(
         queryBuilder: (outletRecord) => outletRecord.where(
