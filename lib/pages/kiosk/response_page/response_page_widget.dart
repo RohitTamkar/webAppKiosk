def1b71465ad6744031fa47b9998bf2a1c7d4a4a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-14 09:06:13 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/kiosk/response_page/response_page_widget.dart
+++ lib/pages/kiosk/response_page/response_page_widget.dart
@@ -78,7 +78,10 @@ class _ResponsePageWidgetState extends State<ResponsePageWidget> {
             actions: [
               TextButton(
                 onPressed: () => Navigator.pop(alertDialogContext),
-                child: Text('Ok'),
+                child: Text(valueOrDefault<String>(
+                  FFAppState().paytmOrderId,
+                  '0',
+                )),
               ),
             ],
           );
